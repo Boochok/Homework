6d7ec6fa3dd189addd6cbff178f843375d9e5323
Daryya Buchok <buchokdaryya@gmail.com>
2017-10-30 01:23:51 +0300

    hw strings task1

--- String/src/hw/string/task1/CrazyLogger.java
+++ String/src/hw/string/task1/CrazyLogger.java
@@ -1,27 +1,29 @@
 package hw.string.task1;
 
-
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 public class CrazyLogger {
     static StringBuilder tape = new StringBuilder();
-    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-YYYY : HH-mm");
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-YYYY : HH-mm-ss");
 
     static void record(String message) {
         Date date = new Date();
-        tape.append(sdf.format(date)).append(" - ").append(message).append(";");
+        tape.append(sdf.format(date)).append(" - ").append(message).append("; ");
     }
-    public static String watchTheTape(String date){
+    public static String watchTheTape(Date date){
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        String fdate = sdf.format(date);
 
-        String[] notes = tape.toString().split(";");
+        String[] notes = tape.toString().split("; ");
 
-        StringBuilder find = new StringBuilder(date + ": \n");
+        StringBuilder find = new StringBuilder(fdate + ": \n");
 
         for (String log : notes) {
-            if(log.startsWith(date)){
+            if (log.startsWith(fdate)){
                 String[] s = log.split(" : ");
-                find.append(s[1]).append("\n");
+                find.append(s[1]+"\n");
             }
         }
         return find.toString();
--- String/src/hw/string/task1/Main.java
+++ String/src/hw/string/task1/Main.java
@@ -1,11 +1,16 @@
 package hw.string.task1;
 
+import java.util.Date;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         CrazyLogger.record("first message");
-        System.out.println(CrazyLogger.tape);
+        Thread.sleep(1000);
         CrazyLogger.record("message2");
+        Thread.sleep(1000);
         CrazyLogger.record("massage3");
+        System.out.println(CrazyLogger.tape.toString());
+        System.out.println(CrazyLogger.watchTheTape(new Date()));
 
     }
 }
