bc2f4476d06e51380248eb75ac3f016d189132a7
Daryya Buchok <buchokdaryya@gmail.com>
2017-10-29 14:04:29 +0300

    hw strings task3(regexp)

--- String/src/hw/string/task3_regexp/PicturesAtArticle.java
+++ String/src/hw/string/task3_regexp/PicturesAtArticle.java
@@ -5,33 +5,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import org.jsoup.*;
-import org.jsoup.nodes.*;
-import org.jsoup.select.*;
 
 
 public class PicturesAtArticle {
     public static void main(String[] args) throws IOException {
         PicturesAtArticle pictures = new PicturesAtArticle();
-//        Document doc = Jsoup.parse(readHTML("String\\resources\\AnalyzedFile.html", "windows-1251"));
-//        String text = doc.text();
-//        System.out.println(text);
-
-        pictures.findThePictures(readHTML("String\\resources\\AnalyzedFile.html", "windows-1251"));
+        String text = pictures.readHTML("String\\resources\\AnalyzedFile.html", "windows-1251");
+        List<String> picturesRef = pictures.findThePictures(text);
+        System.out.println(picturesRef.toString());
+        System.out.println(isAscendingOrder(picturesRef));
     }
 
-    public void findThePictures(String text) throws IOException {
+    public List<String> findThePictures(String text) throws IOException {
         List<String> pictureRefList = new ArrayList<>();
         Pattern pattern = Pattern.compile(
-                "[Рр]ис(\\D{4,5})?\\.? ((\\([а-я,]+\\))|\\d+[\\-][а-я,]+|\\d*\\s?[\\,и]?\\s?\\d+)");
+                "[Рр]ис(\\D{4,5})?\\.? ((\\([а-я,]+\\))|\\d+[\\-][а-я,]+|\\d*\\s?[,и]?\\s?\\d+)");
         Matcher matcher = pattern.matcher(text);
         while(matcher.find()){
             pictureRefList.add(matcher.group());
         }
-        System.out.println(pictureRefList.toString());
+        return pictureRefList;
     }
 
-    public static String readHTML(String path, String charset) throws IOException {
+    public String readHTML(String path, String charset) throws IOException {
         StringBuilder sb = new StringBuilder();
         try(BufferedReader breader = new BufferedReader(new InputStreamReader(
                 new FileInputStream(path), charset))) {
@@ -41,7 +37,7 @@ public class PicturesAtArticle {
         }
         return sb.toString();
     }
-    public boolean isConsistently(List<String> list){
+    public static boolean isAscendingOrder(List<String> list){
         for(int i = 1; i< list.size(); i++){
             if(list.get(i).compareTo(list.get(i-1)) < 0)
                 return false;
